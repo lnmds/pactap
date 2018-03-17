04ef30f370a255c65601d271726047d53eafac94
firestar115 <tsunderebug@tsunderebug.com>
2018-03-16 21:13:10 -0700

    Basic options

--- pactap.go
+++ pactap.go
@@ -3,12 +3,28 @@ package main
 import (
     "fmt"
     "os"
+    "github.com/jessevdk/go-flags"
 )
 
 const VERSION string = "0.0.1"
 
-func usage(){
-    fmt.Printf("usage: pactap [main operators] [...]\n")
+var opts struct {
+    Database bool `short:"D" long:"database" description:"Operate on the package database."`
+    Query bool `short:"Q" long:"query" description:"Query the package database."`
+    Remove bool `short:"R" long:"remove" description:"Remove packages from system."`
+    Sync bool `short:"S" long:"sync" description:"Synchronize packages or groups with remote."`
+    Deptest bool `short:"T" long:"deptest" description:"Check dependencies required for a package."`
+    Upgrade bool `short:"U" long:"upgrade" description:"Upgrade or add packages via a \"remove-then-add\" process."`
+    Files bool `short:"F" long:"files" description:"Query the files database to check which package provides a file."`
+    Version bool `short:"V" long:"version" description:"Display version and exit."`
+
+    DatabasePath string `short:"b" long:"dbpath" description:"Specify an alternative database path to \"~/.pactap\"."`
+    Arch bool `long:"arch" description:"Specify an alternate architecture."`
+    Fancy bool `long:"fancy" description:"Force fancy mode on non-tty systems."` // TODO this is for CLI stuff, will be disabled on detection of non-tty
+    Config string `long:"config" description:"Specify an alternate config file."`
+    Debug bool `long:"debug" description:"Display debug messages. Use when reporting bugs."`
+
+    // TODO Big options; ones specific to D Q R S T U F
 }
 
 func version(){
@@ -18,12 +34,26 @@ func version(){
 
 func main(){
     if len(os.Args) < 2 {
-        usage()
+        os.Args = append(os.Args, "-h")
+    }
+
+    _, err := flags.Parse(&opts)
+    if err != nil {
         return
     }
 
+    if opts.Version {
+        version()
+        return
+    }
+
+    // TODO: Use the opts for something
+    fmt.Printf("Opt Database %t\n", opts.Database)
+    fmt.Printf("Opt Query %t\n", opts.Query)
+    fmt.Printf("Opt Remove %t\n", opts.Remove)
+
     // TODO: We should start reading our config files
-    conf := ReadConfig("/home/luna/.pactap/config.toml")
+    conf := ReadConfig("~/.pactap/config.toml")
     fmt.Println(*conf)
     // TODO: We should start reading our db files
 }
